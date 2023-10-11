f82e5cbefa8cbae350697d14d7ebbdf28799d632
Jess Alejo <tbki.jea@gmail.com>
2023-10-11 10:56:54 +0800

    How to use Column & Row Widgets for Layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.white,
         body: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.all(20.0),
-            padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
-            height: 60.0,
-            width: 200.0,
-            color: Colors.white,
-            child: const Text("Hello World"),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100,
+                color: Colors.red,
+                child: const Center(child: Text("Red")),
+              ),
+              Container(
+                height: 100,
+                color: Colors.orange,
+                child: const Center(child: Text("Orange")),
+              ),
+              Container(
+                height: 100,
+                color: Colors.yellow,
+                child: const Center(child: Text("Yellow")),
+              ),
+              Container(
+                height: 100,
+                color: Colors.green,
+                child: const Center(child: Text("Green")),
+              ),
+              Container(
+                height: 100,
+                color: Colors.blue,
+                child: const Center(child: Text("Blue")),
+              ),
+              Container(
+                height: 100,
+                color: Colors.indigo,
+                child: const Center(child: Text("Indigo")),
+              ),
+              Container(
+                height: 100,
+                color: Colors.purple,
+                child: const Center(child: Text("Purple")),
+              ),
+            ],
           ),
         ),
       ),

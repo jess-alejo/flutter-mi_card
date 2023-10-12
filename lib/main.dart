cb79b4ac575c0839de9c2807baa82417a60ebe79
Jess Alejo <tbki.jea@gmail.com>
2023-10-12 08:59:56 +0800

    Incorporating Custom Fonts in Your Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,25 @@ class MyApp extends StatelessWidget {
                 backgroundImage:
                     AssetImage("assets/images/avatar_jess_alejo.png"),
               ),
+              Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
+              Text(
+                "JESS ALEJO",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Orbitron",
+                    letterSpacing: 3),
+              ),
+              Text(
+                "SOFTWARE DEVELOPER",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 11.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Orbitron",
+                    letterSpacing: 2.5),
+              ),
             ],
           ),
         ),

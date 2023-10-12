0824e6935a5f76f49141158e5102f052481c6ee9
Jess Alejo <tbki.jea@gmail.com>
2023-10-12 11:06:50 +0800

    Flutter Card & ListTile Widgets

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
-# flutter_mi_card
+# Mi Card
 
-A new Flutter project.
+## How to Build Beautiful UIs with Flutter Widgets
 
-## Getting Started
+The Complete Flutter Development Bootcamp with Dart
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![Final Project](screenshot.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import "dart:ui";
-
 import "package:flutter/material.dart";
 
 void main() {
@@ -13,84 +11,104 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(
+          title: const Center(
+              child: Text(
+            "About Me",
+            style: TextStyle(fontFamily: "Orbitron", letterSpacing: 2),
+          )),
+          backgroundColor: Colors.deepPurple.shade600,
+        ),
         body: SafeArea(
-          child: Column(
-            children: [
-              const CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 50,
-                backgroundImage:
-                    AssetImage("assets/images/avatar_jess_alejo.png"),
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.deepPurple.shade700,
+                  Colors.deepPurple.shade900
+                ],
               ),
-              const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
-              const Text(
-                "JESS ALEJO",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "Orbitron",
-                    letterSpacing: 3),
-              ),
-              const Text(
-                "SOFTWARE DEVELOPER",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 13.5,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: "Orbitron",
-                    letterSpacing: 2.5),
-              ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(25, 20, 25, 10),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.deepPurple.shade100),
-                  borderRadius: const BorderRadius.all(Radius.circular(5)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 50,
+                  backgroundImage:
+                      AssetImage("assets/images/avatar_jess_alejo.png"),
                 ),
-                child: Row(
-                  children: [
-                    const Icon(Icons.phone_android_outlined,
+                const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 0)),
+                const Text(
+                  "JESS ALEJO",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Orbitron",
+                      letterSpacing: 3),
+                ),
+                const Text(
+                  "SOFTWARE DEVELOPER",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13.5,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "Orbitron",
+                      letterSpacing: 2.5),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+                  child: Text(
+                    "Experienced Full-Stack Developer | C# | Ruby on Rails | Expanding Mobile and Front-End Skills in React, HTML, CSS, JavaScript, and UI/UX Design",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "Montserrat",
+                        height: 1.5,
+                        backgroundColor: Colors.transparent),
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: Divider(color: Colors.deepPurple.shade100),
+                ),
+                Card(
+                  color: Colors.deepPurple.shade100,
+                  margin: const EdgeInsets.only(
+                      left: 25, right: 25, bottom: 5, top: 20),
+                  child: const ListTile(
+                      leading: Icon(Icons.phone_android_outlined,
+                          color: Colors.deepPurple, size: 24),
+                      title: Text(
+                        "+63 XXX XXX 2360",
+                        style: TextStyle(
+                            color: Colors.deepPurple,
+                            fontFamily: "Orbitron",
+                            fontSize: 14,
+                            letterSpacing: 1.5),
+                      )),
+                ),
+                Card(
+                  color: Colors.deepPurple.shade100,
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                  child: ListTile(
+                    leading: const Icon(Icons.email_outlined,
                         color: Colors.deepPurple, size: 24),
-                    const SizedBox(width: 15),
-                    Text(
-                      "+63 915 391 2360",
-                      style: TextStyle(
-                          color: Colors.deepPurple.shade900,
-                          fontFamily: "Orbitron",
-                          fontSize: 14,
-                          letterSpacing: 1.5),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 25),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.deepPurple.shade100),
-                  borderRadius: const BorderRadius.all(Radius.circular(5)),
-                ),
-                child: Row(
-                  children: [
-                    Icon(Icons.email_outlined,
-                        color: Colors.deepPurple.shade400, size: 24),
-                    const SizedBox(width: 15),
-                    Text(
+                    title: Text(
                       "jess.programmer@gmail.com",
                       style: TextStyle(
                           color: Colors.deepPurple.shade900,
                           fontFamily: "Orbitron",
                           fontSize: 14,
                           letterSpacing: 1.5),
-                    )
-                  ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
